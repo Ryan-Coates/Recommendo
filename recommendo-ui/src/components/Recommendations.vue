6d942c515de15f9ade2eedb77d1cc6458b265e88
ryancoates89 <ryan.coates@tombola.com>
2020-08-11 21:58:45 +0100

    wip preview structure

--- recommendo-ui/src/components/Recommendations.vue
+++ recommendo-ui/src/components/Recommendations.vue
@@ -2,6 +2,19 @@
 <template>
     <div id="example">
         <h1>{{ type }}</h1>
+
+        <div v-if="isLoading" class="article-preview">Loading articles...</div>
+        <div v-else>
+          <div v-if="articles.length === 0" class="article-preview">
+            No articles are here... yet.
+          </div>
+          <RwvArticlePreview
+            v-for="(article, index) in articles"
+            :article="article"
+            :key="article.title + index"
+          />
+          <VPagination :pages="pages" :currentPage.sync="currentPage" />
+        </div>
         <p>{{ info }}</p>
     </div>
 </template>
