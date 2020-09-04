de76366d99247cc44d5f3ddb729d93ce128272f6
ryancoates89 <ryan.coates@tombola.com>
2020-09-04 11:11:48 +0100

    form sumbit

--- recommendo-ui/src/components/RecommendationForm.vue
+++ recommendo-ui/src/components/RecommendationForm.vue
@@ -1,11 +1,34 @@
 <template>
-    <form v-on:submit.prevent="recommendationSubmit(name, description)" class="" action="#" method="post" :name=type data-netlify="true" >
+<div>
+    <form v-on:submit.prevent="recommendationSubmit(name, description)" class="" action="#" method="post" :name=type data-netlify="true" data-netlify-honeypot="bot-field" >
     <input v-model="name" type="text" name="name" value="" placeholder="name">
     <input v-model="description" type="text" name="description" value="" placeholder="description">
     <button type="submit" name="button">Add {{type}}</button>
 </form>
+  <form
+    name="ask-question"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        type="radio"
+        name="panelist"
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
+  </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'RecommendationForm',
   props: {
@@ -14,12 +37,31 @@ export default {
   data () {
     return {
       name: '',
-      description: ''
+      description: '',
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
     }
   },
   methods: {
     recommendationSubmit (name, description) {
       this.$emit('addRecommendation', name, description)
+      const axiosConfig = {
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+      axios.post(
+        '/',
+        this.encode({
+          'form-name': this.type,
+          ...this.form
+        }),
+        axiosConfig)
+    },
+    encode (data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&')
     }
 
   }
