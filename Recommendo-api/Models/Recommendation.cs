91d81c530a925ae1fc296a5bec525c9384acc83c
Ryan Coates <ryancoates89@hotmail.co.uk>
2021-02-21 21:33:51 +0000

    replac ewith guid

--- Recommendo-api/Models/Recommendation.cs
+++ Recommendo-api/Models/Recommendation.cs
@@ -6,6 +6,7 @@ namespace Recommendo_api.Models
     //todo: some model validation
     public partial class Recommendation
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; } 
