a725b4728f5016ae95ede9b708639e71f8bae767
Ryan Coates <ryancoates89@hotmail.co.uk>
2021-03-21 22:50:23 +0000

    lint!1

--- recommendo-ui/.eslintrc.js
+++ recommendo-ui/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    //'@vue/typescript/recommended'
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
