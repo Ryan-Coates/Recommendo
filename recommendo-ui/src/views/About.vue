79bb0bc9033c0c81e09785116c855fdbc6593fbd
Ryan-Coates <Ryan.Coates@tombola.com>
2021-11-25 21:33:28 +0000

    logo and text tweak

--- recommendo-ui/src/views/About.vue
+++ recommendo-ui/src/views/About.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="about">
     <h1>Welcome to Recommendo!</h1>
-    <p>So this is really an excuse for me to get comfortable with vuejs and always a place I can capture recommendations from friends and family</p>
-    <p>Lots of work to do to get this app fully functional, but we have something working now</p>
-    <ul>
-      <li>running every for free which gives some limitations</li>
-      <li>when you add a recommendation it goes to a backend that will update it later, you will see the recommendation for you but others will need to wait a bit, this will be changed in the future</li>
-      <li>Can only proccess 100 recommendation a month :(, when we hit the limit I'll add a notification</li>
-    </ul>
-    <p>everything is open souce, you can see the code, add issues and contribute <a href="https://github.com/Ryan-Coates/Recommendo">here</a></p>
+    <p>Have you ever done something and wanted to tell your friends and families?</p>
+    <p>Have you ever forgotten a recommendation while looking through netflix?</p>
+    <p>feels like a solve-able problem doesn't it?</p>
+    <p>You lucky few are in on the ground floor, try this out please give me feedback!</p>
+    <p>if this is useful I'll take your feedback and build something properly :)</p>
+    <p>Everything is open souce, you can see the code, add issues and contribute <a href="https://github.com/Ryan-Coates/Recommendo">here</a></p>
   </div>
 </template>
